9d0e6aae2a44c8b6ef50b71591f141ed10970019
Hanseliahp <eliahanshpurwanto@gmail.com>
2026-07-04 19:03:54 +0700

    Added csv data untuk input data dan sebagainya dan menyempurnakan fungsi tambah barang

--- main.c
+++ main.c
@@ -28,131 +28,120 @@
         {"T013", "Masako", "Komoditas", 1000, 100}
     };
 
-int validasiIsi(char inputChar[], int validasi){
+int validasiIsi(char inputChar[]){
     if(inputChar[0] == '\0'){
-        validasi = 0;
         printf("Tidak boleh kosong!\n");
         return 0;
     }
-    validasi = 1;
     return 1;
 }
 
-int validasiInteger(int *outputInteger, char inputChar[], int validasi){
+int validasiInteger(char inputChar[], int *outputInteger){
     char *endptr;
-
     *outputInteger = strtol(inputChar, &endptr, 10);
-
-    // Jika masih ada karakter selain newline atau null, input tidak valid
     if (*endptr != '\n' && *endptr != '\0') {
-        validasi = 0;
+        printf("Input harus berupa angka!\n");
         return 0;
     }
-    validasi = 1;
     return 1;
 }
 
 int tambahBarang(){
-    int validasi = 0;
-    char pilihanChar[10], hargaChar[50], stokChar[20], kode[10], nama[100], kategori[20], pilihanChar[10];
-    int pilihanInt, hargaInt, stokInt, pilihanInt;
+    char pilihanChar[10], hargaChar[50], stokChar[20], kode[10], nama[100], kategori[20];
+    int pilihanInt = 0, hargaInt = 0, stokInt = 0, validasi = 0;
     FILE *file;
-
-    file = fopen("/data/barang.csv", "r");
-    if (file == NULL) {
-        printf("File tidak dapat ditemukan, kembali ke menu utama\n");
-        return 0;
-    } else {
-        printf("File berhasil dibuka\n");
+    do  {
+        do
+        {   
+            do
+            {
+                printf("Selamat datang di menu tambah barang\n");
+                printf("Masukkan kode barang (maks 4 karakter): ");
+                fgets(kode, sizeof(kode), stdin);
+                kode[strcspn(kode, "\n")] = 0;
+                if (!validasiIsi(kode)){
+                    break;
+                }
+            } while (!validasiIsi(kode));
+            do
+            {
+                printf("Masukkan nama barang (maks 30 karakter): ");
+                fgets(nama, sizeof(nama), stdin);
+                nama[strcspn(nama, "\n")] = 0;
+                if(!validasiIsi(nama)){
+                    break;
+                }
+            } while (!validasiIsi(nama));
+            do
+            {
+                printf("Masukkan kategori barang (maks 10 karakter)\n");
+                printf("Kategori yang tersedia: Komoditas, Minuman, Snack\n");
+                printf(": ");
+                fgets(kategori, sizeof(kategori), stdin);
+                kategori[strcspn(kategori, "\n")] = 0;
+                if(!validasiIsi(kategori)){
+                    break;
+                }
+            } while (!validasiIsi(kategori));
+            do  {
+                printf("Masukkan harga barang: ");
+                fgets(hargaChar, sizeof(hargaChar), stdin);
+                hargaChar[strcspn(hargaChar, "\n")] = 0;
+                if (!validasiInteger(hargaChar, &hargaInt)) {
+                    printf("Input harga tidak valid. Silakan masukkan angka.\n");
+                }
+            } while (!validasiInteger(hargaChar, &hargaInt));
+            do  {
+                printf("Masukkan stok barang: ");
+                fgets(stokChar, sizeof(stokChar), stdin);
+                stokChar[strcspn(stokChar, "\n")] = 0;
+                if (!validasiInteger(stokChar, &stokInt)) {
+                    printf("Input stok tidak valid. Silakan masukkan angka.\n");
+                }
+            } while (!validasiInteger(stokChar, &stokInt));
+            printf("Pastikan input data sudah benar\n");
+            printf("Apakah anda ingin mengulang input data? (1. Ya / 0. Tidak) : ");
+            fgets(pilihanChar, sizeof(pilihanChar), stdin);
+            pilihanChar[strcspn(pilihanChar, "\n")] = 0;
+            pilihanInt = atoi(pilihanChar);
+            switch (pilihanInt)
+            {
+            case 1:
+                printf("Mengulang penginputan data\n");
+                break;
+            case 0:
+                system("cls");
+                printf("Menginput data\n");
+                break;            
+            default:
+                printf("Pilihan tidak valid!\n");
+                break;
+            }   
+        } while (pilihanInt != 0);
+        file = fopen("data/barang.csv", "a");
+        fprintf(file, "%s,%s,%s,%d,%d\n", kode, nama, kategori, hargaInt, stokInt);
         fclose(file);
-    }
-    do {
-        printf("Selamat datang di menu tambah barang\n");
-        printf("Silahkan pilih : \n");
-        printf("1. Tambah barang\n");
-        printf("99. Kembali ke menu utama\n");
+        printf("Data berhasil ditambahkan ke database\n");
+        printf("Apakah anda ingin mengulang input data?\n");
+        printf("1. Ya\n");
+        printf("0. Tidak\n");
         printf("Pilihanmu : ");
         fgets(pilihanChar, sizeof(pilihanChar), stdin);
-        pilihanInt = atoi(pilihanChar);
-        switch (pilihanInt)
-        {
+        pilihanChar[strcspn(pilihanChar, "\n")] = 0;
+            switch (pilihanInt)
+            {
             case 1:
-                do {
-
-                    system("cls");
-                    printf("Silahkan input barang\n");
-                    do {
-                        printf("Masukkan kode barang (Contoh : T001): ");
-                        fgets(kode, sizeof(kode), stdin);
-                        if (validasiIsi(kode, validasi)){
-                            break;
-                        }
-                    } while(validasi != 1);
-                    do {
-                        printf("Masukkan nama barang : ");
-                        fgets(nama, sizeof(nama), stdin);
-                        if (validasiIsi(nama, validasi)){
-                            break;
-                        }
-                    } while (validasi != 1);
-                    do {
-                        printf("Masukkan kategori barang (Contoh : Beras 5 KG): ");
-                        fgets(kategori, sizeof(kategori), stdin);
-                        if (validasiIsi(kategori, validasi)){
-                            break;
-                        }
-                    } while (validasi != 1);
-                    do  {
-                        printf("Masukkan harga barang (HANYA DALAM ANGKA SAJA) (Contoh : 10000): ");
-                        fgets(hargaChar, sizeof(hargaChar), stdin);
-                        if (validasiInteger(hargaChar, &hargaInt, validasi)){
-                            break;
-                        }
-                    } while(validasi != 1);
-                    do {
-                        printf("Masukkan jumlah stok barang : ");
-                        fgets(stokChar, sizeof(stokChar), stdin);
-                        if (validasiInteger(stokChar, &stokInt, validasi)){
-                            break;
-                        }
-                    } while (validasi != 1);
-                    validasi = 0;
-                    do
-                    {
-                        printf("Apakah input sudah valid?\n");
-                        printf("1 - Ya\n");
-                        printf("0 - Tidak, ulangi lagi\n");
-                        print("Pilihanmu : ");
-                        fgets(pilihanChar, sizeof(pilihanChar), stdin);
-                        if(validasiInteger(pilihanChar, &pilihanInt, validasi)){
-                            break;
-                        }
-                    } while (validasi != 1);
-                    file = fopen("/data/barang.csv", "a");
-                    if (file == NULL) {
-                        printf("File tidak dapat dibuka, kembali ke menu utama\n");
-                        return 0;
-                    } else {
-                        fprintf(file, "%s,%s,%s,%d,%d\n", kode, nama, kategori, hargaInt, stokInt);
-                        fclose(file);
-                        printf("Barang berhasil ditambahkan\n");
-                    }
-                    printf("Apakah mau menambah barang lagi?\n");
-                    printf("1 - Ya\n");
-                    printf("0 - Tidak, ulangi lagi\n");
-                    print("Pilihanmu : ");
-                } while (validasi != 1);
-
+                system("cls");
                 break;
-            case 99:
+            case 0:
+                system("cls");
                 printf("Kembali ke menu utama\n");
-                break;
+                break;            
             default:
-                printf("Pilihan Invalid\n");
+                printf("Pilihan tidak valid!\n");
                 break;
-        }
-    } while (pilihanInt != 99);
-
+            }
+    } while (pilihanInt != 0);
 }
 
 int menuDatabase(){
@@ -197,6 +186,24 @@ int main() {
     int validasi = 0;
     char pilihanChar[10];
     system("cls");
+    FILE *file;
+
+    file = fopen("data/barang.csv", "r");
+    if (file == NULL) {
+        printf("File tidak dapat dibuka, kembali ke menu utama\n");
+        exit(1);
+    }
+    fclose(file);
+    fopen("data/barang.csv", "a");
+    fseek(file, 0, SEEK_END);
+    long size = ftell(file);
+    if (size == 0){
+        fprintf(file, "Kode,Nama,Kategori,Harga,Stok\n");
+        for (int i = 0; i < 13; i++) {
+            fprintf(file, "%s,%s,%s,%d,%d\n", produk[i].id, produk[i].nama, produk[i].kategori, produk[i].harga, produk[i].kuan);
+        }
+    }   
+    fclose(file);
     do
     {
         printf("Selamat datang di sistem supermarket!\n");
